06a5c2b23ca1a4630233e2eb2681650aa6771253
Aaditya Chakravarty <ac36500@gmail.com>
2020-11-02 18:59:31 +0530

    Simplified Code

---
+++ README.md
@@ -0,0 +1,23 @@
+# Bouncer Bot
+
+Just another bot for discord.
+
+## Installation and Running
+
+`git clone https://github.com/aaditya/bouncer-bot`
+
+`cd bouncer-bot`
+
+`npm i`
+
+`npm start`
+
+## Setup
+
+Make a config.json file in root with content
+
+```json
+{
+    "BOT_TOKEN": "<your_discord_token>"
+}
+```
--- index.js
+++ index.js
@@ -16,35 +16,49 @@ client.on("message", function (message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
+  // message.guild.id
+
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
 
   if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  } else if (command === "sum") {
-    const numArgs = args.map(x => parseFloat(x));
-    const sum = numArgs.reduce((counter, x) => counter += x);
-    message.reply(`The sum of all the arguments you provided is ${sum}!`);
-  } else if (command === "source") {
-    message.reply(`You can check out the code at : https://github.com/aaditya/bouncer-bot`);
+    message.reply(`** :hourglass: ${Math.abs(timeTaken)}ms **`);
+  }
+
+  if (command === "source") {
+    message.reply(`You can check out the code at https://github.com/aaditya/bouncer-bot`);
   }
 });
 
 app.get('/', (req, res) => {
-  res.json(client.channels.cache);
+  let channels = client.channels.cache.map(({ type, name, id, guild }) => ({ guild: guild.id, data: { type, name, id }, }));
+  return res.status(200).json({
+    data: client.guilds.cache.map((server) => ({
+      id: server.id,
+      name: server.name,
+      channels: channels.filter(d => d.guild === server.id).map(c => c.data)
+    }))
+  });
 });
 
-app.post('/:channelId', (req, res) => {
-  let target = client.channels.cache.find(c => c.id === req.params.channelId);
-  target.send(req.body.message);
-  res.json({
-    success: true,
-    message: req.body.message,
-    channel: req.params.channelId,
-  })
-})
+app.post('/', (req, res) => {
+  let { message, channelId } = req.body;
+  if (!message || !channelId) {
+    return res.status(400).json({
+      message: "Invalid Fields"
+    })
+  }
+
+  let target = client.channels.cache.find(c => c.id === channelId);
+  target.send(message);
+
+  return res.status(200).json({
+    channel: channelId,
+    message,
+  });
+});
 
 client.login(config.BOT_TOKEN);
 
